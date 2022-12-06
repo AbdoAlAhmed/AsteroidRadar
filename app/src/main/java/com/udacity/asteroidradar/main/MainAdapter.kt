593323e45cf23436aa2a971dba8dc8247db61848
Abdo <Abdelrhman201912@gmail.com>
2022-12-06 19:06:56 +0200

    Finish project

---
+++ app/src/main/java/com/udacity/asteroidradar/main/MainAdapter.kt
@@ -0,0 +1,48 @@
+package com.udacity.asteroidradar.main
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.udacity.asteroidradar.Asteroid
+import com.udacity.asteroidradar.databinding.ItemListBinding
+
+class MainAdapter (val onClickListener:Onclick) : ListAdapter<Asteroid, MainAdapter.ViewHolder>(DiffcallBack) {
+
+    object DiffcallBack : DiffUtil.ItemCallback<Asteroid>() {
+        override fun areContentsTheSame(oldItem: Asteroid, newItem: Asteroid): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areItemsTheSame(oldItem: Asteroid, newItem: Asteroid): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    class ViewHolder(private val bind: ItemListBinding) :
+        RecyclerView.ViewHolder(bind.root) {
+        fun binding(asteroid: Asteroid) {
+            bind.asteroid = asteroid
+            bind.executePendingBindings()
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(ItemListBinding.inflate(LayoutInflater.from(parent.context)))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val asteroid = getItem(position)
+        holder.binding(asteroid)
+        holder.itemView.setOnClickListener {
+            onClickListener.onClick(asteroid)
+        }
+
+    }
+    class Onclick(val OnClickListener :(asteroid:Asteroid)->Unit){
+        fun onClick(asteroid: Asteroid) = OnClickListener(asteroid)
+    }
+}
--- app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -4,30 +4,39 @@ import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.observe
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
+    private  val adapter: MainAdapter? = null
 
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
         val application = requireNotNull(activity)
         val viewModelFactory = MainViewModelFactory(application.application)
-        viewModel = ViewModelProvider(this,viewModelFactory)[MainViewModel::class.java]
+        viewModel = ViewModelProvider(this, viewModelFactory)[MainViewModel::class.java]
 
         val binding = FragmentMainBinding.inflate(inflater)
         binding.lifecycleOwner = this
-
         binding.viewModel = viewModel
+        binding.asteroidRecycler.adapter = MainAdapter(MainAdapter.Onclick{
+            findNavController().navigate(MainFragmentDirections.actionShowDetail(it))
+        })
+
 
         setHasOptionsMenu(true)
 
